4dcd164fa8bd63a36a9c3aa8bc77236f4d262c95
Schliffen <nehrani@gmail.com>
2020-03-05 20:54:25 +0300

    build results

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ali/ProjLAB/BMXcodes
+# Build directory: /home/ali/ProjLAB/BMXcodes/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
