ee727fbb00fba0202597d70bb38845702cc017ca
Ali <nehrani@gmail.com>
2020-03-24 19:34:45 +0300

    removing

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ali/ProjLAB/BMXcodes
-# Build directory: /home/ali/ProjLAB/BMXcodes/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(){
-    std::cout<<"initializing ..."<<std::endl;
-    std::cout<<"Ending ..."<<std::endl;
-    
-    return 0;
-}
